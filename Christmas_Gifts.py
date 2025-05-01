c4f8f403563eb54d69b1e278bda7f9a282e6ec89
Logan2345676543 <pet0072@copperfieldcollege.vic.edu.au>
2025-05-01 13:31:42 +1000

    Add files via upload

---
+++ Christmas_Gifts.py
@@ -0,0 +1,6 @@
+# asks which 3 gifts they want the most
+gift1 = input("What gift would you want the most? ")
+gift2 = input("What gift would you want second most? ")
+gift3 = input("What gift would you want third most? ")
+gift_Total = gift1 + " " + gift2 + " " + gift3
+print(gift_Total)
