55b39a26402d2906389d18944c4a28b986009c38
Logan2345676543 <pet0072@copperfieldcollege.vic.edu.au>
2025-05-01 13:00:09 +1000

    Add files via upload

---
+++ SchoolLunch.py
@@ -0,0 +1,4 @@
+LunchMoney = int(input("how much money did you spend on lunch? "))
+Money = int(input("How much money did you have at the start of the day? "))
+MoneyLeft = Money-LunchMoney
+print("You have", LunchMoney, "left")
