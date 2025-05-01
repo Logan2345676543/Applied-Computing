6223d4ffd87b1f4a5b7fe4bbcfe1ff3de78a0709
Logan2345676543 <pet0072@copperfieldcollege.vic.edu.au>
2025-05-01 13:05:23 +1000

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,4 @@
+x = int(input("What is the length of the room in meters? "))
+y = int(input("What is the width of the room in meters? "))
+area = x*y
+print("The area of the room is", area,"meters")
