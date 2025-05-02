686b3085c8f3287aedcdeeec695a922961946f33
Logan2345676543 <pet0072@copperfieldcollege.vic.edu.au>
2025-05-02 15:07:09 +1000

    Add files via upload

---
+++ ferretoffer.py
@@ -0,0 +1,5 @@
+choice = input("Which homegroup are you in? ")
+if choice in("XB1","Xk2","Xb1","Xk3","XK2","XK3"):
+     print("You get a discount.")
+else:
+     print("You do not get a discount.")
