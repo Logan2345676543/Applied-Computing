9bbc0ee2e080159930f670275b15085667a91acb
Logan2345676543 <pet0072@copperfieldcollege.vic.edu.au>
2025-05-03 18:47:11 +1000

    Add files via upload

---
+++ Happy_message.py
@@ -0,0 +1,7 @@
+happy = int(input("How happy are you on a sacle from 1 to 10? "))
+if happy <= 3:
+    print("I hope you feel better.")
+elif happy >= 8:
+    print("Thats great enjoy your day.")
+elif happy:
+    print("Thats ok enjoy your day.")
